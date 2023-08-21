b420ed2f9cc325d58620ab386fca97192a5bb07f
Matheus <matheusm1999@live.com>
2023-08-21 17:58:43 -0300

    Base Controller error solved

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
     [ApiController]
-    [Route("[api/cotroller]")]
+    [Route("[controller]")]
     public class BaseApiController : ControllerBase
     {
         
